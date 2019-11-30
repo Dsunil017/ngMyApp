1bf77040fca566f2e9b82ae92a43fff4c61f8f13
dsunil017 <dsunil017@gmail.com>
2019-11-30 13:09:01 +0530

    ne app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,45 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-start",
-  template: "<h1>Angular Welcomes You..!</h1>"
+  templateUrl: "./app.component.html",
+  styles: [`./app.component.css`]
 })
-export class FirstComponent {}
+export class FirstComponent {
+  heading = "Angular welcomes you...!";
+
+  mobParts = [
+    {
+      id: 1001,
+      name: "MOBILE screen",
+      desc: "its a moto g4 plus screen",
+      inStock: 12,
+      price: 1200,
+      isOnline: true,
+      addToCart: true,
+      stl: "hotpink",
+      country: "Australia"
+    },
+    {
+      id: 1002,
+      name: "mobile screen",
+      desc: "its a moto g4 plus screen",
+      inStock: 125,
+      price: 1200,
+      isOnline: false,
+      addToCart: true,
+      stl: "orange",
+      country: "Germany"
+    },
+    {
+      id: 1003,
+      name: "Mobile screen",
+      desc: "its a moto g4 plus screen",
+      inStock: 0,
+      price: 1200,
+      isOnline: true,
+      addToCart: false,
+      stl: "green",
+      country: "India"
+    }
+  ];
+}
