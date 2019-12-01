c57d19bc624c50b4c2103a1092b42466cc8535c6
dsunil017 <dsunil017@gmail.com>
2019-12-01 12:14:58 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FirstComponent } from "./app.component";
+import { SampleDirectiveDirective } from './sample-directive.directive';
 @NgModule({
-  declarations: [FirstComponent],
+  declarations: [FirstComponent, SampleDirectiveDirective],
   imports: [BrowserModule],
   bootstrap: [FirstComponent]
 })
